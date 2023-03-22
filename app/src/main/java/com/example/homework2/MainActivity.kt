7d19aefbcd0fccfdf22d843aea57e391a374f0d6
Artsem Stsepanenka <asstep@mail.ru>
2023-03-22 21:25:12 +0300

    create 2_2

--- app/src/main/java/com/example/homework2/MainActivity.kt
+++ app/src/main/java/com/example/homework2/MainActivity.kt
@@ -8,28 +8,17 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-    companion object{
-        val EXTRA_MESSAGE = "putMainActivity"
-    }
     private var mCount = 0
     lateinit var  mShowCount: TextView
-    lateinit var buttonToast: Button
     lateinit var buttonCount: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mShowCount = findViewById(R.id.show_count)
-        buttonToast = findViewById(R.id.button_toast)
         buttonCount = findViewById(R.id.button_count)
         buttonCount.setOnClickListener {
             mCount++;
             mShowCount.text = mCount.toString();
-        }
-        buttonToast.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            val message = mCount.toString()
-            intent.putExtra(EXTRA_MESSAGE, message)
-            startActivity(intent)
         }
     }
 
