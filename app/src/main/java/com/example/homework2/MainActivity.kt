eac168b68ea8c650b7b109fbd67b50f19aeaf647
Artsem Stsepanenka <asstep@mail.ru>
2023-03-23 14:36:23 +0300

    finish 2_3 codelab

--- app/src/main/java/com/example/homework2/MainActivity.kt
+++ app/src/main/java/com/example/homework2/MainActivity.kt
@@ -1,35 +1,74 @@
 package com.example.homework2
 
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat
 
 
 class MainActivity : AppCompatActivity() {
-    private var mCount = 0
-    lateinit var mShowCount: TextView
-    lateinit var buttonCount: Button
+    lateinit var webSiteEditText: EditText
+    lateinit var locationEditText: EditText
+    lateinit var shareEditText: EditText
+    lateinit var siteButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mShowCount = findViewById(R.id.show_count)
-        buttonCount = findViewById(R.id.button_count)
-        buttonCount.setOnClickListener {
-            mCount++;
-            mShowCount.text = mCount.toString();
+        webSiteEditText = findViewById(R.id.website_edittext)
+    }
+
+    fun openWebsite(view: View) {
+        val url = webSiteEditText.text.toString()
+        val webpage: Uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        try {
+            startActivity(intent)
+        }catch (e: ActivityNotFoundException) {
+            Toast.makeText(
+                this,
+                "Cannot handle this",
+                Toast.LENGTH_LONG).show()
         }
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt("count", mCount)
+
+    fun openLocation(view: View) {
+        locationEditText = findViewById(R.id.location_edittext)
+        val location = locationEditText.text.toString()
+        val locationUri = Uri.parse("geo:0,0?q=$location")
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = locationUri
+        }
+        try {
+            startActivity(intent)
+        }catch (e: ActivityNotFoundException) {
+            Toast.makeText(
+                this,
+                "Cannot handle this",
+                Toast.LENGTH_LONG).show()
+        }
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        mCount = savedInstanceState.getInt("count")
-        mShowCount.text = mCount.toString()
+    fun shareText(view: View) {
+        shareEditText = findViewById(R.id.share_edittext)
+        val text = shareEditText.toString()
+        val mimeType = "text/plain"
+        ShareCompat.IntentBuilder
+            .from(this)
+            .setType(mimeType)
+            .setChooserTitle("Share this text with:")
+            .setText(text)
+            .startChooser()
     }
+
+
 }
